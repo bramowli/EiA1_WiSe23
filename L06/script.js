7c50f678d7084cbc7f49688b0872fc193cc69f02
bramowli <149688574+bramowli@users.noreply.github.com>
2023-12-03 11:26:05 +0100

    done

--- L06/script.js
+++ L06/script.js
@@ -15,13 +15,13 @@ if (yeti == "scream") {
     alert("Not being able to contain your fear any longer you open your mouth and let out a piercing scream. The creature flinches and, to your surprise, screams right back at you. So now you are both standing frozen, looking at eachother and screaming in fear.\nSuddenly a third sound joins in your screaming match. A low grumbling you feel in your whole body. It is growing closer, still you keep standing there as if your feet are tied to the ground.\nBoth of you have stopped screaming by now, but you are still looking at eachother in fear. Not fear of eachother anymore, but fear of the inevitably grumbling. Your eyes are still locked, as a wave of white uproots the trees next to you and burries you both.");
 }
 else if (yeti == "closer") {
-    alert("hug");
+    alert("This is a once in a lifetime chance. Of course you have to examine the unknown being closer.\nIt curiously tilts it's head to the side as you take a step forward.\nYou are almost in its reach by now and it slowly starts to lift it's arm. You keep going anyways.\nA heavy paw softly settles on your shoulder. Holding your breath you lift your head and lock eyes with the mystery in front of you. Hypnotizing dark eyes hold your gaze as the paw shifts to your upper back and starts to pull you in closer. Your face gets pressed into the slightly wet, but surprisingly soft furr of the creatures chest. \nFinally breathing out, you let yourself relax and be completely encased by this unlikely embrace.");
 }
 else if (fall == "get up") {
-    alert("get lost");
+    alert("This is not the place to die!\nPain is shooting through your ribs as you struggle to stand. Finally you arrive in an upright position and are able to keep on going.\nStep after step, you fight your way through the snow and the pain.\nYou are determined to find your way out of this, but in your hasty escape you completely lost your orientation and the growing amount of snowflakes is not helping. Every step takes longer until you can barely keep lifting your feet.\nIt seems that this is the place to die now.\nYou've heard that freezing is not the worst way to go anyways.");
 }
 else if (fall == "stay down") {
-    alert("savior");
+    alert("That's it.\nWhat use does it even have to try?\nEven if you get up, you are just going to freeze anyways.\nWhy not here when you're already laying down?\nYour thoughts are drifting aimlessly. Who might find your body?\nDid you turn off the stove? Could chicken swim, if you gave them shoes?\nThrough your increasingly abstract thoughts a sound of slow, rhythmic crunching drifts in your ears. It's getting louder. Is death coming to pick you up?\nSomeone IS picking you up. Could that really be death? It feels a bit too warm and soft for that. With quite some effort you manage to open your eyes.\nOh, it's Bigfoot.\nOf course it is.\nYou smile as you close your eyes again.");
 }
 else {
     alert("Come on, it's not that hard. Try again!");
